47c3d395f2dd59d3e8f0d853d57804ea72bb365d
shubhamkumar13 <kumar.shubham1312@gmail.com>
2020-05-11 14:54:57 +0530

    wip

--- LU_decomposition_multicore_task.ml
+++ LU_decomposition_multicore_task.ml
@@ -21,49 +21,20 @@ open SquareMatrix
 let chunk_size = 32
 let pool = T.setup_pool ~num_domains:(num_domains - 1)
 
-let distribution size =
-  let rec loop n d acc =
-    if d = 1 then n::acc
-    else if n < chunk_size then n::acc
-    else if n/d < chunk_size then loop (n - chunk_size) (d-1) (chunk_size::acc)
-    else
-      let w = n / d in
-      loop (n - w) (d - 1) (w::acc)
-  in
-  List.rev (loop size num_domains []) 
-
-
-let lup a0 =
+let lup pool (a0 : float array) =
   let a = copy a0 in
-  let aux k s e =
-    (* Printf.printf "[%d] aux k=%d s=%d e=%d\n%!" d k s e; *)
-    for row = s to (pred e) do
-      let factor = get a row k /. get a k k in
-      for col = k + 1 to mat_size-1 do
-          set a row col (get a row col -. factor *. (get a k col))
-      done;
-      set a row k factor
-    done
-  in
   for k = 0 to (mat_size - 2) do
-    let mine, rest =
-      match distribution (mat_size - 2 - k) with
-      | [] -> failwith "impossible"
-      | x::xs -> x,xs
-    in
-    (* Printf.printf "\nmine=%d " mine; *)
-    (* List.iter (fun r -> Printf.printf "%d " r) rest; *)
-    let sum = ref (k + mine) in
-    let pr_list =
-      List.map (fun size ->
-        let begin_ = !sum in
-        sum := !sum + size;
-        let end_ = !sum in
-        T.async pool (fun _ -> aux k begin_ end_)) rest
-    in
-    aux k k (mine + k);
-    List.iter (fun pr -> T.await pool pr) pr_list;
-  done;
+  T.parallel_for pool 
+  ~chunk_size:chunk_size 
+  ~start:(k + 1) 
+  ~finish:(mat_size  -1)
+  ~body:(fun row ->
+    let factor = get a row k /. get a k k in
+    for col = k + 1 to mat_size-1 do
+      set a row col (get a row col -. factor *. (get a k col))
+      done;
+    set a row k factor )
+  done ;
   a
 
 let () =
